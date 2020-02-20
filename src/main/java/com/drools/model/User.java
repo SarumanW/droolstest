0e315066a64dddbeb52e4596522b98843693c2b9
Olha Kozlova <olha.kozlova@netcracker.com>
2020-02-20 18:58:41 +0200

    Initial commit

---
+++ src/main/java/com/drools/model/User.java
@@ -0,0 +1,16 @@
+package com.drools.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private boolean fatLoss;
+
+    private List<String> forbiddenProducts;
+}
